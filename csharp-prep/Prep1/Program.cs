856c5b8de9dad03e2ed8028f35e1ce8cfaf87e92
jwatkinsm <49845925+jwatkinsm@users.noreply.github.com>
2026-06-08 16:51:14 -0400

    w1p1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,18 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+
+        Console.Write("What is your first name?");
+        string first= Console.ReadLine();
+
+        Console.Write("What is your last name?");
+        string last= Console.ReadLine();
+
+        Console.WriteLine($"So, your name is {last}, {first} {last}.");
     }
 }
