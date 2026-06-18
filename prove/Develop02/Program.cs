24356a62f213d6ebd54a1029342baa0d539c309a
jwatkinsm <49845925+jwatkinsm@users.noreply.github.com>
2026-06-18 19:14:00 -0400

    project required code

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,56 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Journal theJournal = new Journal();
+        PromptGenerator promptGenerator = new PromptGenerator();
+        string choice = "";
+
+        Console.WriteLine("Welcome to your Journal project");
+
+        while (choice != "5")
+        {
+            Console.WriteLine("Please select one of the following choices:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            Console.Write("What would you like to do? ");
+            choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                string prompt = promptGenerator.GetRandomPrompt();
+                Console.WriteLine(prompt);
+                Console.Write("> ");
+                string response = Console.ReadLine();
+
+                Entry newEntry = new Entry();
+                newEntry.date = DateTime.Now.ToShortDateString();
+                newEntry.promptText = prompt;
+                newEntry.entryText = response;
+
+                theJournal.AddEntry(newEntry);
+            }
+            else if (choice == "2")
+            {
+                theJournal.DisplayAll();
+            }
+            else if (choice == "3")
+            {
+                Console.Write("What is the filename? ");
+                string filename = Console.ReadLine();
+                theJournal.LoadFromFile(filename);
+            }
+            else if (choice == "4")
+            {
+                Console.Write("What is the filename? ");
+                string filename = Console.ReadLine();
+                theJournal.SaveToFile(filename);
+            }
+        }
+
+
+
     }
 }
